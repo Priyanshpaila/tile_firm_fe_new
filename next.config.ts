b8a03054ba41d1f4fbe7344babd2fb15e0a022c4
admin rrispat <software@rrispat.com>
2026-04-11 17:11:40 +0530

    ..

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "http", hostname: "**" },
-      { protocol: "https", hostname: "**" }
-    ]
-  }
+      { protocol: "https", hostname: "**" },
+    ],
+  },
 };
 
 export default nextConfig;
